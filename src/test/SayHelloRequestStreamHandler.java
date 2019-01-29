5b57bd8932f0f7b8f8c45093e8c6351932ca6f02
Abdulrahman Salim <abdulrahman.salim@ucdconnect.ie>
2019-01-29 15:29:29 +0000

    Hello World

---
+++ src/test/SayHelloRequestStreamHandler.java
@@ -0,0 +1,33 @@
+package test;
+
+import java.util.HashSet;
+import java.util.Set;
+import com.amazon.speech.speechlet.Speechlet;
+import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
+
+
+public class SayHelloRequestStreamHandler extends SpeechletRequestStreamHandler {
+
+    private static final Set<String> supportedApplicationIds;
+
+    static {
+        /*
+         * This Id can be found on https://developer.amazon.com/edw/home.html#/
+         * "Edit" the relevant Alexa Skill and put the relevant Application Ids
+         * in this Set.
+         */
+        supportedApplicationIds = new HashSet<String>();
+
+        //supportedApplicationIds.add("[Add your Alexa Skill ID and then uncomment and ]";
+        supportedApplicationIds.add("amzn1.ask.skill.aa625910-08d7-4b64-94f4-e34c81ae367c");
+        System.out.println("Supported app ids : " + supportedApplicationIds);
+    }
+
+    public SayHelloRequestStreamHandler() {
+        super(new test.SayHelloSpeechlet(), supportedApplicationIds);
+    }
+
+    public SayHelloRequestStreamHandler(Speechlet speechlet, Set<String> supportedApplicationIds) {
+        super(speechlet, supportedApplicationIds);
+    }
+}
